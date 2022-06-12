20ca6b94c27e5bb03c87cb13ce51e4f6f5ca2d1b
Brian <brianbatz03@gmail.com>
2022-06-12 14:21:31 -0600

    Multiplicación en python

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+def multiplica(x, y):
+    return x * y
+
 def run():
-    print("Hola Mundo!!!")
+    print("\tMULTIPLICACIÓN DE DOS NÚMEROS")
+    n1 = int(input("Ingresa el primer número a multiplicar: "))
+    n2 = int(input("Ingresa el segundo número a multiplicar: "))
+    print("El resultado de la multiplicación es: " + str(multiplica(n1, n2)))
 
 if __name__ == "__main__":
     run()
