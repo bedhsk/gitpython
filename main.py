c19e339402a76971bc71179d63e9cf1e44ac2ba2
Brian <brianbatz03@gmail.com>
2022-06-12 19:46:58 -0600

    División de dos números en python

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-# Función para realizar multiplicaciones
-def multiplica(x, y):
-    return x * y
+# Función para realizar divisiones
+def divide(x, y):
+    return x / y
 
 # Función principal del programa
 def run():
-    print("\tMULTIPLICACIÓN DE DOS NÚMEROS")
-    n1 = int(input("Ingresa el primer número a multiplicar: "))
-    n2 = int(input("Ingresa el segundo número a multiplicar: "))
-    print("El resultado de la multiplicación es: " + str(multiplica(n1, n2)))
+    print("\tDIVISIÓN DE DOS NÚMEROS")
+    n1 = int(input("Ingresa el primer número a dividir: "))
+    n2 = int(input("Ingresa el segundo número a dividir: "))
+    print("El resultado de la devisión es: " + str(divide(n1, n2)))
 
 if __name__ == "__main__":
     run()
